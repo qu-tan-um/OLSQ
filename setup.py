497c7a21e307d9801e9ce5d9e6c9f9599d013485
tbcdebug <tbcdebug@outlook.com>
2021-05-03 19:47:44 -0700

    0.0.4release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="olsq",
-    version="0.0.3",
+    version="0.0.4",
     license = "BSD",
     author="Daniel Bochen Tan",
     author_email="bctan@cs.ucla.edu",
