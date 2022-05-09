a4fbd103fb6c6ede4d09846b4efe63a11ef9a5fd
Daniel Bochen Tan <tbcdebug@outlook.com>
2022-05-09 14:24:50 -0700

    setup 0.0.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="olsq",
-    version="0.0.4",
+    version="0.0.4.1",
     license = "BSD",
     author="Daniel Bochen Tan",
     author_email="bctan@cs.ucla.edu",
